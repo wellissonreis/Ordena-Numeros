6d80bfce02ff18701343d95a9011024c85b0a052
Only-Well <wellissonsilvareis28@hotmail.com>
2023-09-06 14:09:55 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿static List<int> SortNumbers(List<int> numeros)
+{
+    for (int i = numeros.Count -1; i >= 1; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (numeros[j] > numeros[j + 1])
+            {
+                int aux = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = aux;
+            }
+        }
+    }
+
+    return numeros;
+}
+
+List<int> numeros = new List<int> { 5, 2, 9, 1, 5, 6, 8, 18, 2, 56, 65, 26, 28, 26, 23, 24, 22 };
+List<int> numerosOrdenados = SortNumbers(numeros);
+
+foreach (var numero in numerosOrdenados)
+{
+    Console.Write(numero + " "); 
+}
+
